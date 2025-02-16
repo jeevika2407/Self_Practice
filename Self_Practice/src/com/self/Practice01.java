27709ddb113b110a3e0ca742204e267a4d49f610
jeevika <kirubajeevika@gmail.com>
2025-02-16 19:34:44 +0530

    second commit

--- Self_Practice/src/com/self/Practice01.java
+++ Self_Practice/src/com/self/Practice01.java
@@ -3,6 +3,7 @@ package com.self;
 public class Practice01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 }
